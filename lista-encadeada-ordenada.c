1b719ef800b71b76f48517367f8d654c60a46080
Lahat-Herev <pedroarthuralvesdeoliveira@hotmail.com>
2022-11-07 22:04:35 -0300

    Finished exercise of linked lists

--- lista-encadeada-ordenada.c
+++ lista-encadeada-ordenada.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct elemento
 {
@@ -55,11 +56,26 @@ Lista buscaLista(Lista lista, int elemento)
 
 Lista insereLista(Lista lista, int elemento)
 {
-    Lista novo;
+    Lista anterior, proximo, novo;
+    anterior = lista;
     novo = malloc(sizeof(struct elemento));
-    novo->dado = elemento;
-    novo->proximo = lista;
-    return (novo);
+    proximo = lista;
+    while (proximo != NULL && elemento > proximo->dado)
+    {
+        anterior = proximo;
+        proximo = proximo->proximo;
+    }
+    if (proximo != anterior)
+    {
+        anterior->proximo = novo;
+    }
+    else
+    {
+        lista = novo;
+    }
+    anterior->proximo = novo;
+    novo->proximo = proximo;
+    return (lista);
 }
 
 Lista retiraLista(Lista lista, int elemento)
@@ -90,9 +106,60 @@ Lista retiraLista(Lista lista, int elemento)
     return (lista);
 }
 
+void menu()
+{
+    printf("\nDigite o numero equivalente ao que deseja fazer: \n");
+    printf("\n1 - Inserir elemento\n");
+    printf("\n2 - Retirar elemento \n");
+    printf("\n3 - Buscar elemento na lista \n");
+    printf("\n4 - Exibir o conteúdo da lista \n");
+    printf("\n5 - Contar elementos da lista \n");
+}
+
 int main()
 {
+    int elemento;
+    int operacao;
     Lista lista;
+    Lista auxiliar;
     lista = criaLista();
+    do
+    {
+        menu();
+        scanf("%d", &operacao);
+        fflush(stdin);
+        switch (operacao)
+        {
+        case 1:
+            printf("Entre com o item: ");
+            scanf("%d", &elemento);
+            lista = insereLista(lista, elemento);
+            break;
+        case 2:
+            printf("Digite o item que gostaria de remover: ");
+            scanf("%d", &elemento);
+            lista = retiraLista(lista, elemento);
+            break;
+        case 3:
+            printf("Digite o item que gostaria de buscar na lista: ");
+            scanf("%d", &elemento);
+            auxiliar = buscaLista(lista, elemento);
+            if (auxiliar == NULL)
+            {
+                printf("Nao existe. \n");
+            }
+            printf("Elemento %d encontrado. \n", elemento);
+            break;
+        case 4:
+            imprimeLista(lista);
+            break;
+        case 5:
+            printf("A lista tem %d elementos", contaLista(lista));
+            break;
+        default:
+            printf("Essa opção não existe.");
+            break;
+        }
+    } while ((operacao > 0) && (operacao < 6));
     return 0;
 }
